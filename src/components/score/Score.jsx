a41d0c7c47759e64df49f9b1a6fc0a1e394dc73d
FacundoArdizzi <ardizzifacundo@gmail.com>
2021-11-04 14:35:18 -0300

    score structure and styles

--- src/components/score/Score.jsx
+++ src/components/score/Score.jsx
@@ -7,9 +7,15 @@ const Score = () => {
 
   return (
     <div className={styles.score}>
-      <span>{score.white}</span>
-      <div />
-      <span>{score.black}</span>
+      <div>
+        <span>{score.white}</span>
+        <p>white</p>
+      </div>
+      <div className={styles.divider} />
+      <div>
+        <span>{score.black}</span>
+        <p>black</p>
+      </div>
     </div>
   );
 };
